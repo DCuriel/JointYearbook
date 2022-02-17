1b1ef9b3872cfbb647da90a1e2fcdde7897ffb9b
boradibra <boradibra1@gmail.com>
2022-02-17 16:28:46 -0500

    Added NextActivity

--- app/src/main/java/com/example/jointyearbook/MainActivity.java
+++ app/src/main/java/com/example/jointyearbook/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.jointyearbook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener((View.OnClickListener) this);
+
+
+    }
+
+    public void onClick(View v){
+        //((TextView)findViewById(R.id.textView5)).setText("Text changed");
+        Intent i = new Intent(getApplicationContext(), NextActivity.class);
+        startActivity(i);
     }
 }
